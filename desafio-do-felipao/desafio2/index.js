c8d81dfba7ac3011a3b67e3cf5db397c951857df
MetzkerDeveloper <walerianoadm@gmail.com>
2024-01-14 16:56:14 -0300

    feat: Desafio 2, Nivel de Heroi do curso do Potência Tech iFood - Programação do Zero ofertado pela DIO

---
+++ desafio-do-felipao/desafio2/index.js
@@ -0,0 +1,27 @@
+function calcularNivel(vitorias, derrotas) {
+  let saldoVitorias = vitorias - derrotas;
+
+  let nivel;
+
+  if (saldoVitorias < 10) {
+    nivel = "Ferro";
+  } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+    nivel = "Bronze";
+  } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+    nivel = "Prata";
+  } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+    nivel = "Ouro";
+  } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+    nivel = "Diamante";
+  } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+
+  return `O Heroi tem um saldo de ${saldoVitorias} vitórias e está no nível de ${nivel}`;
+}
+
+let resultado = calcularNivel(100, 10);
+
+console.log(resultado);
